8ecc500695bf0e821144267017e7a1b4b8b65df7
Gabriel Radzki <gabriel.radzki@gmail.com>
2021-01-14 22:34:21 -0300

    New Post - Bash Script

---
+++ _posts/2021-01-14Reverse Engineering Sagemcom F@ST 5302.md
@@ -0,0 +1,6 @@
+---
+title: Reverse Engineering Sagemcom F@ST 5302
+date: 2021-01-14 22:33:31 -0300
+categories: []
+tags: []
+---
---
+++ new_post.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+DATE=`date '+%Y-%m-%d'`
+DATETIME=`date '+%Y-%m-%d %H:%M:%S -0300'`
+POST_TITLE=$1
+
+FILENAME="${DATE}${POST_TITLE}.md"
+FILE_PATH="./_posts/"${FILENAME}
+
+echo -en "---\ntitle: ${POST_TITLE}\ndate: ${DATETIME}\ncategories: []\ntags: []\n---\n" > "${FILE_PATH}"
