696fe58bdc3d1dfd3f5a918c387debd81c9a83cf
Gabriel Radzki <gabriel.radzki@gmail.com>
2021-01-14 22:41:00 -0300

    No need for "layout: post" as it is already set default

--- new_post.sh
+++ new_post.sh
@@ -8,4 +8,4 @@ POST_TITLE=$1
 FILENAME="${DATE}${POST_TITLE}.md"
 FILE_PATH="./_posts/"${FILENAME}
 
-echo -en "---\nlayout: post\ntitle: ${POST_TITLE}\ndate: ${DATETIME}\ncategories: []\ntags: []\n---\n" > "${FILE_PATH}"
+echo -en "---\ntitle: ${POST_TITLE}\ndate: ${DATETIME}\ncategories: []\ntags: []\n---\n" > "${FILE_PATH}"
